9819ba6ee58bb125789d3268b9dde363916d1ff8
laurenproctor <laurenproctor32@gmail.com>
2014-04-04 10:28:21 -0400

    setting up app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YtVideos::Application.config.secret_key_base = '985a2f577161e9dfaf6d3119487b3bd640c844f4251132806d4c9d72c477642478ced5b28521a36a37212450907f67a8de14157090b0e5c0c85bc3231bcb6ed6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YtVideos::Application.config.session_store :cookie_store, key: '_yt_videos_session'
